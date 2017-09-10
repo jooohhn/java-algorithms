6d621e27700f5880693f5701ab99a5e7e3a04231
John Tran <jptran318@gmail.com>
2017-09-10 16:23:17 -0700

    Restructured sorting tests to have random and edge cases

--- tests/sorting/BubbleSortTest.java
+++ tests/sorting/BubbleSortTest.java
@@ -2,12 +2,28 @@ package sorting;
 
 import static org.junit.Assert.assertArrayEquals;
 
+import helpers.HelperFunctions;
+import java.util.Arrays;
 import org.junit.Test;
 
 public class BubbleSortTest {
 
   @Test
-  public void bubbleSort() throws Exception {
+  public void generalCases() throws Exception {
+    int[] arr1 = HelperFunctions.getRandomIntegerArray(0, 1000, 1000);
+    int[] arr1Sorted = Arrays.copyOf(arr1, 1000);
+    Arrays.sort(arr1Sorted);
+    assertArrayEquals(arr1Sorted, BubbleSort.bubbleSort(arr1));
+
+    int[] arr2 = HelperFunctions.getRandomIntegerArray(0, 100000, 10000);
+    int[] arr2Sorted = Arrays.copyOf(arr2, 10000);
+    Arrays.sort(arr2Sorted);
+    assertArrayEquals(arr2Sorted, BubbleSort.bubbleSort(arr2));
+  }
+
+
+  @Test
+  public void edgeCases() throws Exception {
     int[] intArr = {4, 9, 1, 34, 12, 6, 5, 18, 51, 21, -5, -3, 89, -2};
     int[] sortedIntArr = {-5, -3, -2, 1, 4, 5, 6, 9, 12, 18, 21, 34, 51, 89};
     assertArrayEquals(sortedIntArr, BubbleSort.bubbleSort(intArr));
--- tests/sorting/QuickSortTest.java
+++ tests/sorting/QuickSortTest.java
@@ -2,12 +2,28 @@ package sorting;
 
 import static org.junit.Assert.assertArrayEquals;
 
+import helpers.HelperFunctions;
+import java.util.Arrays;
 import org.junit.Test;
 
 public class QuickSortTest {
 
   @Test
-  public void quickSort() throws Exception {
+  public void generalCases() throws Exception {
+    int[] arr1 = HelperFunctions.getRandomIntegerArray(0, 1000, 1000);
+    int[] arr1Sorted = Arrays.copyOf(arr1, 1000);
+    Arrays.sort(arr1Sorted);
+    assertArrayEquals(arr1Sorted, QuickSort.quickSort(arr1));
+
+    int[] arr2 = HelperFunctions.getRandomIntegerArray(0, 100000, 10000);
+    int[] arr2Sorted = Arrays.copyOf(arr2, 10000);
+    Arrays.sort(arr2Sorted);
+    assertArrayEquals(arr2Sorted, QuickSort.quickSort(arr2));
+  }
+
+
+  @Test
+  public void edgeCases() throws Exception {
     int[] intArr = {4, 9, 1, 34, 12, 6, 5, 18, 51, 21, -5, -3, 89, -2};
     int[] sortedIntArr = {-5, -3, -2, 1, 4, 5, 6, 9, 12, 18, 21, 34, 51, 89};
     assertArrayEquals(sortedIntArr, QuickSort.quickSort(intArr));
